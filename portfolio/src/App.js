f67dee1504509df15f00e452cc5a67e12ccaf493
jrsswish <quodet.johnrey@gmail.com>
2025-06-26 23:13:06 -0600

    change display type

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className='logo'>
+    <div className='logo' style={{display: "flex"}}>
       <h1 className="name">John Rey Quidet</h1>
-      <div className='options'>
+      <div className='options' style={{display:"flex"}}>
         <h1 className='home'>Home</h1>
         <h1 className='projects'>Projects</h1>
         <h1 className='contacts'>Contacts</h1>
