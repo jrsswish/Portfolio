1554a67903555343021f81b3b387e57c7937124d
jrsswish <quodet.johnrey@gmail.com>
2025-06-26 23:10:20 -0600

    added options at top of portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="App">John Rey Quidet</h1>
+    <div className='logo'>
+      <h1 className="name">John Rey Quidet</h1>
+      <div className='options'>
+        <h1 className='home'>Home</h1>
+        <h1 className='projects'>Projects</h1>
+        <h1 className='contacts'>Contacts</h1>
+      </div>
     </div>
   );
 }
