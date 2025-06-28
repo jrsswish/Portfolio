30197f5dec27d9dfbcea957f2a648a388c0b45d5
jrsswish <quodet.johnrey@gmail.com>
2025-06-28 09:36:03 -0600

    change to js type fie

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRoutes, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.js";
 
 function App() {
   return (
