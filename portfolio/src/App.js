7aa21ebac25243e093f027101d5f38d585376cd4
jrsswish <quodet.johnrey@gmail.com>
2025-06-28 08:20:46 -0600

    positioning for top portion change

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='logo' style={{display: "flex"}}>
-      <h1 className="name">John Rey Quidet</h1>
+    <div className='logo' style={{display: "flex", justifyContent:'space-around', alignItems:'center'}}>
+      <p className="name">John Rey Quidet</p>
       <div className='options' style={{display:"flex", textDecoration:"none"}}>
         <a className='home'>Home</a>
         <a className='projects'>Projects</a>
