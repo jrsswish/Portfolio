4955cb8b0de56b0ffaffa8ef3170a9d4081d5076
jrsswish <quodet.johnrey@gmail.com>
2025-07-20 20:42:00 -0600

    added little bit of description

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -2,14 +2,18 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='logo' style={{display: "flex", justifyContent:'space-around', alignItems:'center'}}>
-      <p className="name">John Rey Quidet</p>
-      <div className='options' style={{display:"flex"}}>
-        <Link className='home' to='/' style={{textDecoration:"none"}}>Home</Link>
-        <Link className='projects' to='/projects' style={{textDecoration:"none"}}>Projects</Link>
-        <Link className='contacts' to='/contacts' style={{textDecoration:"none"}}>Contacts</Link>
+    <div>
+      <div className='logo' style={{display: "flex", justifyContent:'space-around', alignItems:'center'}}>
+        <p className="name">John Rey Quidet</p>
+        <div className='options' style={{display:"flex"}}>
+          <Link className='home' to='/' style={{textDecoration:"none"}}>Home</Link>
+          <Link className='projects' to='/projects' style={{textDecoration:"none"}}>Projects</Link>
+          <Link className='contacts' to='/contacts' style={{textDecoration:"none"}}>Contacts</Link>    
+        </div>
       </div>
+      <div className="description"> I am a 3rd year university student from University of Saskatchewan. I am interested in learning machine learning algorithms and data related field.</div>
     </div>
+
   );
 }
 
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,5 +1,5 @@
 function Projects() {
-
+  
 }
 
 export default Projects;
